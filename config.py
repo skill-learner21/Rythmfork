7fdce107914e9c86e2ab53c2fbbc6cba6ec3d770
Lost Venom <149289075+LostVenom@users.noreply.github.com>
2024-03-30 23:09:44 -0700

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/sanju9636/LOFIPRONEW",
+    "https://github.com/LostVenom/LOFI",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
