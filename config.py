2e16f6cce319513610a5448957ad61f8a08a6ed5
Lost Venom <149289075+LostVenom@users.noreply.github.com>
2024-03-30 23:53:47 -0700

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/LostVenom/LOFI",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
