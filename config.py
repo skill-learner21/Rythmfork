ba979589c550e77f0491aea80a906879cecfde6d
Sanju <141401968+sanju9636@users.noreply.github.com>
2024-03-31 13:39:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/LostVenom/LOFI",
+    "https://github.com/sanju9636/LOFI_MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
